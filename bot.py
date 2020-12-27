33d1b34903cd06f9d0d8af8588bb442beb28cf23
deucemane <deucemane@protonmail.com>
2020-12-26 19:34:51 -0500

    Added a command skeleton
    Added a command for when the bot is called with "!"

--- bot.py
+++ bot.py
@@ -6,10 +6,22 @@ client = commands.Bot(command_prefix = "!")
 
 @client.event
 
-#when the bot successfully gathers all info
+#asyncronous function - (you don't have to wait for it to finish. Once it's started, you can go back to something else) when the bot successfully gathers all info
 async def on_ready():
 	print("bot is ready")
 
+@client.command()
+async def WLR(context):
+	await context.send("T__T Rokkstar++ MaDe !! </33")
+
+@client.event
+async def on_member_join(member):
+	print(f"{member} made it in...")
+
+@client.event
+async def on_member_remove(member):
+	print(f"Smell ya later {member}!")
+
 #calls Controller
-client.run('NzY4ODgyMDQ4MDcyMDI0MDk3.X5G7NQ.h0ShLojdiDCAeium9MSot461TRY')
-#this token was reset after Discord scanned and found it here.
+client.run('NzY4ODgyMDQ4MDcyMDI0MDk3.X5G7NQ.xFpgfH-0IpJNTAJKRpBMmZeX3wQ')
+#might have to change token every upload
